6cb73c8d170634b810e25e06b6f82584f5858254
Suryakant Sharma <suryasharma2901@gmail.com>
2020-03-11 20:58:13 +0530

    Updates

---
+++ precache-manifest.015ee5a3cbb8f81989d898ef59dab5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb3790b4f7ad443c3e242d2fa12ad7c",
+    "url": "/Bus-stop-crowdsourcing/index.html"
+  },
+  {
+    "revision": "309a459150732ce0c7eb",
+    "url": "/Bus-stop-crowdsourcing/static/css/main.32dc4123.chunk.css"
+  },
+  {
+    "revision": "b4b04acf9474189c57ff",
+    "url": "/Bus-stop-crowdsourcing/static/js/2.bae1ce09.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/Bus-stop-crowdsourcing/static/js/2.bae1ce09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309a459150732ce0c7eb",
+    "url": "/Bus-stop-crowdsourcing/static/js/main.8397068f.chunk.js"
+  },
+  {
+    "revision": "4650574a3f6f4219f135",
+    "url": "/Bus-stop-crowdsourcing/static/js/runtime-main.3e3043a9.js"
+  }
+]);
